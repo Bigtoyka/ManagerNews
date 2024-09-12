4def9d56a916e81b8ceb3de7603d4a33b2d2d28b
Bigtoyka <ru.gosha@mail.ru>
2024-09-12 23:41:27 +0300

    start project

---
+++ src/main/kotlin/News.kt
@@ -0,0 +1,9 @@
+data class News (
+    val id: Long,
+    val title: String,
+    val place: String,
+    val description: String,
+    val siteUrl: String,
+    val favoritesCount: Int,
+    val commentsCount: Int
+)
